19a2cd979c21fedf5734fc32d5bb7ec6e16c0bf9
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-07-27 16:30:23 -0600

    Got input to generate li elements correctly

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,23 @@
 const apiKey = "4c7ab1e6de8e858867888fc23a8524ec";
+
+let cityInputEl = document.querySelector(".cityinput");
+let cityUlEl = document.querySelector(".cities");
+
+let searchBtn = document.querySelector(".search");
+
+function printCities(city) {
+  let cityLi = document.createElement("li");
+  cityUlEl.appendChild(cityLi);
+  cityLi.textContent = city;
+  cityLi.classList.add("list-group-item");
+  cityLi.classList.add("list-group-item-dark");
+}
+
+function handleInput(event) {
+  event.preventDefault();
+  let cityInput = cityInputEl.value;
+  printCities(cityInput);
+  cityInputEl.value = "";
+}
+
+searchBtn.addEventListener("click", handleInput);
